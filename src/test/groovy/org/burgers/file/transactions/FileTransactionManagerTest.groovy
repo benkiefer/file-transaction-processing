2368eaf60354a50e9c83a4093a64072270914327
Ben Kiefer <kieferfam@msn.com>
2012-03-29 20:29:52 -0500

    bk: a batch processing example in my tests and some cleanup

--- src/test/groovy/org/burgers/file/transactions/FileTransactionManagerTest.groovy
+++ src/test/groovy/org/burgers/file/transactions/FileTransactionManagerTest.groovy
@@ -1,34 +1,58 @@
 package org.burgers.file.transactions
 
-import org.junit.Test
-import org.junit.Before
-import org.junit.After
+import groovy.mock.interceptor.MockFor
+import junit.framework.TestCase
 
-class FileTransactionManagerTest {
-  FileTransactionManager manager
-  File file
+class FileTransactionManagerTest extends GroovyTestCase{
+    FileTransactionManager manager
+    File file
 
-  @Before
-  void setUp() {
-    manager = new FileTransactionManager()
-    file = File.createTempFile("test", ".txt")
-    manager.workingDirectory = file.parentFile.absolutePath + "/working"
-  }
-
-  @After
-  void tearDown() {
-    file.delete()
-  }
-
-
-  @Test
-  void happyPath() {
-    manager.doInTransaction(file) {OutputStream stream ->
-      1000.times {
-        stream.write('hi\n'.bytes)
-      }
+    void setUp() {
+        manager = new FileTransactionManager()
+        file = File.createTempFile("test", ".txt")
     }
-    assert file.readLines().size() == 1000
-  }
+
+    void tearDown() {
+        file.delete()
+    }
+
+    void happyPath() {
+        manager.doInTransaction(file) {OutputStream stream ->
+            1000.times {
+                stream.write('hi\n'.bytes)
+            }
+        }
+        assert file.readLines().size() == 1000
+    }
+
+    void test_rollBackOnFailure() {
+        manager.doInTransaction(file) {OutputStream stream ->
+            1000.times {
+                stream.write('hi\n'.bytes)
+                if (it == 100) {
+                    throw new RuntimeException("test kaboom!!!")
+                }
+            }
+        }
+        assert file.readLines().size() == 0
+    }
+
+    void test_rollBackOnFailure_multiple_batches() {
+        assert "test kaboom!!!" == shouldFail(RuntimeException){
+            5.times {
+                if (it == 3) throw new RuntimeException("test kaboom!!!")
+                processABatch()
+            }
+        }
+        assert file.readLines().size() == 3000
+    }
+
+    private void processABatch(){
+        manager.doInTransaction(file) {OutputStream stream ->
+            1000.times {
+                stream.write('hi\n'.bytes)
+            }
+        }
+    }
 
 }
